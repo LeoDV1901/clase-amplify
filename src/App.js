2de825863527318de9cbbd3fb337395f5f8b238a
LeoDV1901 <157930536+LeoDV1901@users.noreply.github.com>
2025-02-15 10:06:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Desarrollo TEST
         </a>
       </header>
     </div>
